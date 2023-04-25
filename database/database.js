21725f84662761ea03e2346c3c8e812d38eb5de3
Luis Eduardo <banedycraft@gmail.com>
2023-04-24 21:48:16 -0300

    current timezone

--- database/database.js
+++ database/database.js
@@ -2,7 +2,8 @@ const Sequelize = require('sequelize');
 //informacoes para conexao com banco
 const connection = new Sequelize('projeto-blog','root','123456',{
     host: 'localhost',
-    dialect: 'mysql'
+    dialect: 'mysql',
+    timezone: '-03:00'
 });
 
 
